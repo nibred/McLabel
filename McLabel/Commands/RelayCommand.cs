6de1af38eeac2160c57c85e881aff5e483c363ad
nibred <oleksii.derbin@gmail.com>
2023-03-15 14:48:31 +0100

    Added commands in mvvm

---
+++ McLabel/Commands/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using McLabel.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McLabel.Commands
+{
+    internal class RelayCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object parameter) => _execute.Invoke(parameter);
+    }
+}

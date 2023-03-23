7ad187aa5069768e22f73bf20a73d305e22934fe
nibred <oleksii.derbin@gmail.com>
2023-03-23 19:08:13 +0100

    Date/time visibility fix

---
+++ McLabel/Utils/Converters/StringVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace McLabel.Utils.Converters
+{
+    internal class StringVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string data = value.ToString();
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
